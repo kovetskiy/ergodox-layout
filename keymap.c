5e2bdc30ebd8907b1c8a4054efb6da8383f2e5f3
Egor Kovetskiy <e.kovetskiy@gmail.com>
2018-09-18 19:56:52 +0300

    scratch it

--- keymap.c
+++ keymap.c
@@ -7,62 +7,32 @@
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
 
-#define LANG_SWITCH LALT(KC_LSFT)
-
-#if KEYLOGGER_ENABLE
-# ifdef AUTOLOG_ENABLE
-bool log_enable = true;
-# else
-bool log_enable = false;
-# endif
-#endif
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  Esc   | 1  ! | 2  @ | 3  # | 4  $ | 5  % | Lang |           | Esc  | 6  ^ | 7  & | 8  * | 9  ( | 0  ) | Bckspc |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |  Tab   |   Q  |   W  |   E  |   R  |   T  |  L2  |           |  L2  |   Y  |   U  |   I  |   O  |   P  | \   |  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |  Ctrl  |   A  |   S  |  D   |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  | ;  : |  ' "   |
- * |--------+------+------+------+------+------|  L1  |           |  L1  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  | ,  < | . >  | /  ? |  - _   |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  L1  | `  ~ | ' "  | \  | |  L2  |                                       |  {   |  }   | [  { | ]  } |  L1  |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,--------------.       ,---------------.
- *                                        | Home  | End  |       | PgUp  |Insert |
- *                                 ,------|-------|------|       |-------+-------+------.
- *                                 |      |       | LGui |       | Del   |       |      |
- *                                 | Alt  | LGui  |------|       |-------| Enter |Space |
- *                                 |      |       | LGui |       | Bcsp  |       |      |
- *                                 `---------------------'       `----------------------'
- */
+    //
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
 [BASE] = LAYOUT_ergodox(  // layer 0 : default
   // left hand
-  KC_ESC,     KC_1,    KC_2,     KC_3,     KC_4,   KC_5,  LANG_SWITCH,
-  KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,   KC_T,  MO(2),
+  KC_GRV,     KC_1,    KC_2,     KC_3,     KC_4,   KC_5,  KC_ESC,
+  KC_TAB,     KC_Q,    KC_W,     KC_E,     KC_R,   KC_T,  KC_ESC,
   KC_LCTL,    KC_A,    KC_S,     KC_D,     KC_F,   KC_G,
-  KC_LSFT,    KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,  MO(1),
-  MO(1),      KC_GRV,  KC_QUOT,  KC_BSLS,  MO(2),
+  KC_LSFT,    KC_Z,    KC_X,     KC_C,     KC_V,   KC_B,  KC_ESC,
+   KC_ESC,    KC_GRV,  KC_QUOT,  KC_BSLS,  KC_APP,
 
                                                KC_HOME,   KC_END,
-                                                          KC_LGUI,
-                                      KC_LALT, KC_LGUI,   LANG_SWITCH,
+                                                          KC_ESC,
+                                      KC_LALT, KC_ESC,    MO(2),
 
   // right hand
-  KC_ESC,  KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_BSPC,
-  MO(2),   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_BSLS,
-           KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
-  MO(1),   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   KC_MINS,
-                     KC_LCBR,  KC_RCBR,  KC_LBRC,  KC_RBRC,   MO(1),
+  KC_ESC,   KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_BSPC,
+  KC_ESC,   KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      KC_BSLS,
+            KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   KC_QUOT,
+  KC_ESC,   KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   KC_MINS,
+                      KC_LCBR,  KC_RCBR,  KC_LBRC,  KC_RBRC,   MO(1),
 
-  KC_PGUP,  KC_INS,
-  KC_DEL,
-  KC_BSPC,  KC_ENT,    KC_SPC
+  KC_ESC,  KC_INS,
+  KC_ESC,
+  KC_ESC,  KC_DEL,    KC_ENT
 ),
 /* Keymap 1: Symbol Layer
  *
@@ -174,8 +144,9 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 
 // Runs just one time when the keyboard initializes.
 void matrix_init_user(void) {
-
+    xprintf("XXXXXX\n");
 };
+
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
@@ -202,20 +173,5 @@ void matrix_scan_user(void) {
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-#if KEYLOGGER_ENABLE
-  if (log_enable) {
-    uint8_t layer = biton32(layer_state);
-
-    if (layer == BASE) {
-      uprintf(
-        "{ \"keylog\": true, \"col\": %d, \"row\": %d, \"pressed\": %d, \"layer\": \"%s\" }\n",
-        record->event.key.row,
-        record->event.key.col,
-        record->event.pressed,
-        "QWERTY"
-      );
-    }
-  }
-#endif
-  return true;
+    return true;
 }
